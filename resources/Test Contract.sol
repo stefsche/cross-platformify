ec5136b258d04fb12e0600c3cb570d6ce06d4de9
stefsche <58699420+stefsche@users.noreply.github.com>
2022-04-08 14:41:33 +0200

    Created structure of project
    Created DSL grammar and main python file
    
    Added test smart contract and PNGs of the grammar and smart contract dot representations

---
+++ resources/Test Contract.sol
@@ -0,0 +1,23 @@
+@START sol V8
+
+pragma solidity ^0.8.0;
+
+contract Hello {
+    string public message;
+
+    @IF sol V8   
+    bytes1 = foo;
+    function Inbox(string memory initialMessage) public {
+        message = initialMessage;
+    }
+    @END
+
+    string goo;
+
+    @IF sol >=V7
+    byte = foo;
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+    @END
+}
